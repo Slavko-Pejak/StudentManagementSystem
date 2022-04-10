cc58d3665f8ee194b7733dd303b2ae0fcf867595
Slavko-Pejak <slavko.pejak1996@gmail.com>
2022-04-10 16:23:20 +0200

    This a final commit. We have CRUD operations working. Simple Student Management System ready.

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -6,6 +6,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.demo.service.StudentService;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
@@ -27,10 +30,33 @@ public class StudentController {
 	public String createStudenForm(Model model){
 
 		//Create student object to hold student form data
-		//Student student = new Student();
-
-		//model.addAttribute("student", student);
+		Student student = new Student();
+		model.addAttribute("student", student);
 		return "create_student";
+	}
+	@PostMapping("/students")
+	public String saveStudent(@ModelAttribute("student") Student student){
+		studentService.saveStudent(student);
+		return "redirect:/students";
+	}
+
+	@GetMapping("students/edit/{id}")
+	public String editStudentForm(@PathVariable Long id, Model model){
+		model.addAttribute("student", studentService.getStudentById(id));
+		return "edit_student";
+	}
+
+	@PostMapping("students/{id}")
+	public String updateStudent(@PathVariable Long id ,@ModelAttribute("student") Student student){
+		Student oldStudent = studentService.getStudentById(id);
+		student.setId(oldStudent.getId());
+		studentService.updateStudent(student);
+		return "redirect:/students";
+	}
+	@GetMapping("students/{id}")
+	public String deleteStudent(@PathVariable Long id){
+		studentService.deleteStudentById(id);
+		return "redirect:/students";
 	}
 
 }
